60e1ee0c18f43ec4abc125576c5318f6140a291e
sp37777 <sp3777@gmail.com>
2026-01-19 18:16:51 +0200

    Latest

--- backup/unit1.pas
+++ backup/unit1.pas
@@ -14,11 +14,9 @@ type
     ChkUpdate: TCheckBox;
     ChkDefender: TCheckBox;
     EditUsername: TEdit;
-    Label1: TLabel;
     procedure BtnSaveClick(Sender: TObject);
     procedure FormCreate(Sender: TObject);
   private
-    procedure SyncConfig(IsLoading: Boolean);
   public
   end;
 
@@ -29,52 +27,62 @@ implementation
 
 {$R *.lfm}
 
-{ This function handles BOTH reading and writing to avoid duplicating logic }
-procedure TForm1.SyncConfig(IsLoading: Boolean);
+{ TForm1 }
+
+// --- 1. LOAD DEFAULTS WHEN APP OPENS ---
+procedure TForm1.FormCreate(Sender: TObject);
 var
   Ini: TIniFile;
-  ConfigPath: string;
+  DefaultPath: string;
 begin
-  // Set path to the same folder as the EXE
-  ConfigPath := ExtractFilePath(ParamStr(0)) + 'settings.ini';
+  // Look for defaults.ini in the same folder as the exe
+  DefaultPath := ExtractFilePath(ParamStr(0)) + 'defaults.ini';
 
-  Ini := TIniFile.Create(ConfigPath);
+  Ini := TIniFile.Create(DefaultPath);
   try
-    if IsLoading then
-    begin
-      // READING FROM INI TO UI
-      EditUsername.Text := Ini.ReadString('UserConfig', 'UserName', 'Administrator');
-      ChkUpdate.Checked := Ini.ReadBool('Tweaks', 'DisableUpdates', False);
-      ChkDrivers.Checked := Ini.ReadBool('Tweaks', 'InstallDrivers', True);
-    end
-    else
-    begin
-      // WRITING FROM UI TO INI
-      Ini.WriteString('UserConfig', 'UserName', EditUsername.Text);
-      Ini.WriteBool('Tweaks', 'DisableUpdates', ChkUpdate.Checked);
-      Ini.WriteBool('Tweaks', 'InstallDrivers', ChkDrivers.Checked);
-      ShowMessage('Configuration Saved to ' + ConfigPath);
-    end;
+    // Read values: if the file/key doesn't exist, it uses the 3rd parameter
+    ChkDefender.Checked := Ini.ReadBool('Settings', 'InstallDrivers', False);
+    ChkUpdate.Checked := Ini.ReadBool('Settings', 'ApplyUpdates', False);
+    EditUsername.Text       := Ini.ReadString('User', 'UserName', 'DefaultUser');
   finally
     Ini.Free;
   end;
 end;
 
-procedure TForm1.FormCreate(Sender: TObject);
-begin
-  // Load defaults when the app starts
-  SyncConfig(True);
-end;
-
+// --- 2. SAVE CHOICES AND EXIT ---
 procedure TForm1.BtnSaveClick(Sender: TObject);
+var
+  Ini: TIniFile;
+  USBPath, FinalFilePath: string;
 begin
-  SyncConfig(False);
+  if ChkDefender = nil then
+  begin
+    ShowMessage('Error: ChkDefender is Nil! The component is not connected to the form.');
+    Exit;
+  end;
+  // Get the drive letter (e.g., "D:")
+  USBPath := ExtractFileDrive(ParamStr(0));
+
+  // If for some reason ParamStr(0) fails (unlikely), fallback to current dir
+  if USBPath = '' then USBPath := ExtractFileDrive(GetCurrentDir);
+
+  FinalFilePath := IncludeTrailingPathDelimiter(USBPath) + 'final_settings.ini';
+
+  Ini := TIniFile.Create(FinalFilePath);
+  try
+    // Write current UI state
+    Ini.WriteString('User', 'UserName', EditUsername.Text);
+    Ini.WriteBool('Settings', 'InstallDrivers', ChkDefender.Checked);
+    Ini.WriteBool('Settings', 'ApplyUpdates', ChkUpdate.Checked);
+
+    // Save to physical disk
+    Ini.UpdateFile;
+  finally
+    Ini.Free;
+  end;
+
+  // Terminate the application
+  Application.Terminate;
 end;
-
-procedure TForm1.CheckBox1Change(Sender: TObject);
-begin
-
-end;
-
 
 end.
