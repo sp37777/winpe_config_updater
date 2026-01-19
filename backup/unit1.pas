f23874cfe4c8ecd2372c18a6b6be399edcefd451
sp37777 <sp3777@gmail.com>
2026-01-19 18:44:51 +0200

    Add options

--- backup/unit1.pas
+++ backup/unit1.pas
@@ -11,9 +11,13 @@ type
   { TForm1 }
   TForm1 = class(TForm)
     BtnSave: TButton;
-    ChkUpdate: TCheckBox;
-    ChkDefender: TCheckBox;
-    EditUsername: TEdit;
+    DisableCoreIsolation: TCheckBox;
+    DisableEncryption: TCheckBox;
+    DisableLastAccessTime: TCheckBox;
+    DisableUAC: TCheckBox;
+    DisableUpdates: TCheckBox;
+    DisableDefender: TCheckBox;
+    UserName: TEdit;
     procedure BtnSaveClick(Sender: TObject);
     procedure FormCreate(Sender: TObject);
   private
@@ -41,9 +45,13 @@ begin
   Ini := TIniFile.Create(DefaultPath);
   try
     // Read values: if the file/key doesn't exist, it uses the 3rd parameter
-    ChkDefender.Checked := Ini.ReadBool('Settings', 'InstallDrivers', False);
-    ChkUpdate.Checked := Ini.ReadBool('Settings', 'ApplyUpdates', False);
-    EditUsername.Text       := Ini.ReadString('User', 'UserName', 'DefaultUser');
+    UserName.Text := Ini.ReadString('User', 'UserName', 'User');
+    DisableDefender.Checked := Ini.ReadBool('Tweaks', 'DisableDefender', False);
+    DisableUpdates.Checked := Ini.ReadBool('Tweaks', 'DisableUpdates', False);
+    DisableUAC.Checked := Ini.ReadBool('Tweaks', 'DisableUAC', False);
+    DisableLastAccessTime.Checked := Ini.ReadBool('Tweaks', 'DisableLastAccessTime', False);
+    DisableEncryption.Checked := Ini.ReadBool('Tweaks', 'DisableEncryption', False);
+    DisableCoreIsolation.Checked := Ini.ReadBool('Tweaks', 'DisableCoreIsolation', False);
   finally
     Ini.Free;
   end;
@@ -55,25 +63,29 @@ var
   Ini: TIniFile;
   USBPath, FinalFilePath: string;
 begin
-  if ChkDefender = nil then
+  if DisableDefender = nil then
   begin
     ShowMessage('Error: ChkDefender is Nil! The component is not connected to the form.');
     Exit;
   end;
   // Get the drive letter (e.g., "D:")
-  USBPath := ExtractFileDrive(ParamStr(0));
+  USBPath := ExtractFilePath(ParamStr(0));
 
   // If for some reason ParamStr(0) fails (unlikely), fallback to current dir
-  if USBPath = '' then USBPath := ExtractFileDrive(GetCurrentDir);
+  if USBPath = '' then USBPath := ExtractFilePath(GetCurrentDir);
 
   FinalFilePath := IncludeTrailingPathDelimiter(USBPath) + 'final_settings.ini';
 
   Ini := TIniFile.Create(FinalFilePath);
   try
     // Write current UI state
-    Ini.WriteString('User', 'UserName', EditUsername.Text);
-    Ini.WriteBool('Settings', 'InstallDrivers', ChkDefender.Checked);
-    Ini.WriteBool('Settings', 'ApplyUpdates', ChkUpdate.Checked);
+    Ini.WriteString('User', 'UserName', UserName.Text);
+    Ini.WriteBool('Tweaks', 'DisableDefender', DisableDefender.Checked);
+    Ini.WriteBool('Tweaks', 'DisableUpdates', DisableUpdates.Checked);
+    Ini.WriteBool('Tweaks', 'DisableUAC', DisableUAC.Checked);
+    Ini.WriteBool('Tweaks', 'DisableLastAccessTime', DisableLastAccessTime.Checked);
+    Ini.WriteBool('Tweaks', 'DisableEncryption', DisableEncryption.Checked);
+    Ini.WriteBool('Tweaks', 'DisableCoreIsolation', DisableCoreIsolation.Checked);
 
     // Save to physical disk
     Ini.UpdateFile;
